c65d22ee0a82c51b303457014b177517372279a5
Gabriel Baldessar <gb.baldessar@gmail.com>
2018-04-15 23:52:30 -0300

    change test.js

--- teste.js
+++ teste.js
@@ -1,25 +1,31 @@
 var request = require('request');
 
-request('http://localhost:3000/categories', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+function getCategories() {
+	request('http://localhost:3000/categories', function (error, response, body) {
+	  console.log('error:', error); // Print the error if one occurred
+	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	  console.log('body:', body); // Print the HTML for the Google homepage.
+	});
+}
 
-// request.post(
-//     'http://localhost:3000/categories',
-//     { json: { name: 'value',childrenIds:[2] } },
-//     function (error, response, body) {
-//     	console.log('error:', error); // Print the error if one occurred
-//   		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   		console.log('body:', body); // Print the HTML for the Google homepage.
-//     }
-// );
+function getCategoryById(id) {
+	request(('http://localhost:3000/categories/'+id), function (error, response, body) {
+	  console.log('error:', error); // Print the error if one occurred
+	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	  console.log('body:', body); // Print the HTML for the Google homepage.
+	});
+}
 
-// request('http://localhost:3000/categories/1', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
+function post(categoryName, childrenArray) {
+	request.post(
+	    'http://localhost:3000/categories',
+	    { json: { name: categoryName ,childrenIds: childrenArray } },
+	    function (error, response, body) {
+	    	console.log('error:', error); // Print the error if one occurred
+	  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	  		console.log('body:', body); // Print the HTML for the Google homepage.
+	    }
+	);
+}
 
-
+//call teste function here
