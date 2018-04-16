9894e30bd93b14edd39aa4f96c3785448d0aff27
Luiz Motta <luizjoaomotta@gmail.com>
2018-04-16 01:26:16 -0300

    minor organization and documentation

--- store/src/app.js
+++ store/src/app.js
@@ -4,27 +4,26 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate');
 
-// utilizando mongoose para estruturacao do database
-// sendo store o nome dado ao database criado
+// initializing mongoose
 mongoose.connect('mongodb://localhost/store');
 const db = mongoose.connection;
 
-// checa se ha algum erro no db
+// check for a error in the database
 db.on('error', (err) => {
 	throw err;
 });
 
-// inicia app
+// initialize the app
 const app = express();
 
-// importando o modelo da db
+// import the model
 const Category = require('./model/category');
 
-// body parser middleware
+// body-parser middleware
 app.use(bodyParser.urlencoded( {extended : false} ));
 app.use(bodyParser.json());
 
-// express validator middleware
+// express-validator middleware
 app.use(expressValidator({
 	errorFormatter: function(param, msg, value) {
 		let namespace = param.split('.'),
@@ -41,7 +40,7 @@ app.use(expressValidator({
 	}
 }));
 
-// rota GET /categories
+// GET /categories route, send the categories using pagination or not
 app.get('/categories', (req, res) => {
 	if (Object.keys(req.query).length === 0) {
 		sendCategories(req, res);
@@ -50,15 +49,18 @@ app.get('/categories', (req, res) => {
 	}
 });
 
+// send all the categories sorted by id
 function sendCategories(req, res) {
 	Category.find({}, { '__v' : 0 })
 			.sort({ 'id' : 1 })
 			.exec(function(err, categories) {
 		if (err) throw err;
+
 		res.send({ Categories : categories });
 	});
 }
 
+// send the categories based on the query chosed
 function paginateCategories(req, res) {
 	Category.paginate({}, { page : parseFloat(req.query.page), 
 							limit : parseFloat(req.query.limit),
@@ -69,109 +71,103 @@ function paginateCategories(req, res) {
 	});
 }
 
-// rota GET /categories/:id
+// GET /categories/:id route, send the category with the specfic id
 app.get('/categories/:id', (req, res) => {
 	Category.find({'id' : req.params.id}, 
 		          { '_id' : 0, '__v' : 0 }, 
 		          function(err, category) {
 		if (err) throw err;
+
 		res.send({ Category : category});
 	});
 });
 
-// rota POST /categories
+// POST /categories used to add a new category
 app.post('/categories', (req, res) => {
 
-	// checa se algum campo está em branco
+	// check blank field
 	req.checkBody('id', 'IdRequired').notEmpty();
 	req.checkBody('name', 'NameRequired').notEmpty();
 	let errors = req.validationErrors();
 
-	// caso possui algum campo em branco, aponta qual, 
-	// caso contrario procede para salvar a nova categoria
-	if (errors) {
-		invalidField(res, errors);
-	} else {
-		 newCategory(req, res);
+	if (errors) {		
+		responseInvalidField(res, errors);
+	} else {		
+		 createNewCategory(req, res);
 	}
 
 });
 
-// resposta de campos nao preenchidos e mostra quais sao
-function invalidField (res, errors) {
+// response of which field is invalid
+function responseInvalidField (res, errors) {
 	res.send({
 		"ok" : false,
 		"error" : errors
 	});
 }
 
-// cria uma nova categoria e testa se ela possui childrenIds validos
-function newCategory (req, res) {
-	sameIdValidation(req, res)
+// create a new category
+function createNewCategory (req, res) {
+	idValidation(req, res);
 }
 
-// testa se o Id ja esta sendo utilizado por outra categoria
-function sameIdValidation(req, res) {
+// test if the id already exist
+function idValidation(req, res) {
 
 	Category.find({ 'id' : req.body.id }, function(err, idCategory) {
 		if (err) throw err;
 
-		// testa se o objeto retornado pelo find eh diferente de 0
-		// e caso for, o id ja esta sendo utilizado e tera uma resposta
-		// de id invalido, caso contrario, sera verificado se os
-		// childrenIds sao validos.
 		if (Object.keys(idCategory).length !== 0) {
-			invalidIdResponse(res);
+			responseIvalidId(res);
 		} else {
 			childrenIdValidation(req, res);
 		}
 	});
 }
 
-// procura no db uma lista com os childrenId passados e
-// compara o tamanho da lista encontrada no db com a quantidade de
-// childrenIds pasados, caso seja igual a categoria eh salva no db, 
-// caso contrario as categorias passadas são invalidas.
+// test if the IDs in the childrenId field exist
 function childrenIdValidation(req, res) {
 	let childrenId = req.body.childrenId;
 
 	Category.find({ 'id' : childrenId }, function(err, categories) {
 		if (err) throw err;
-		if (Object.keys(categories).length === childrenId.length) {
-			saveInDB(req, res);
+
+		if (Object.keys(categories).length !== childrenId.length) {
+			responseInvalidCategory(res);
 		} else {
-			invalidCategoriesResponse(res);
+			saveNewCategory(req, res);
 		}
 	});
 }
 
-// salva a nova categoria no database
-function saveInDB (req, res) {
+// save the new category in the database
+function saveNewCategory (req, res) {
 	let category = new Category(req.body);
 
 	category.save(function (err) {
 		if (err) throw err;
+
 		res.send({ "ok" : true });
 	});
 }
 
-// respota de Id ja sendo utilizado
-function invalidIdResponse (res) {
+// response for a Id already being used
+function responseIvalidId (res) {
 	res.send({
 		"ok" : false,
 		"error" : "IdAlreadyBeingUsed"
 	});
 }
 
-// resposta de Categorias filhas invalida
-function invalidCategoriesResponse (res) {
+// response for a invalid category
+function responseInvalidCategory (res) {
 	res.send({ 
 		"ok" : false , 
 		"error" : "InvalidCategories"
 	});
 }
 
-// usado para debugar
+// to use postman
 app.listen(3000, () => {
 	console.log('Server started in port 3000...');
 });
