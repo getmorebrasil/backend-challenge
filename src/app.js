32a306c3c0bdd067bb48c13b52ca6cf4f1af6509
Gabriel Baldessar <gb.baldessar@gmail.com>
2018-04-16 19:34:19 -0300

    - Change app.js

--- src/app.js
+++ src/app.js
@@ -8,8 +8,6 @@ var conString = "postgres://postgres:postgres@localhost:5432/backendchallenge";
 var client = new pg.Client(conString);
 client.connect();
 
-var jsonFile = require("../data/jsonFile.json"); 
-
 var app = express();
 app.use(bodyParser.json());
 
@@ -39,14 +37,16 @@ app.get('/categories/:id',function(req,res,next) {
 })
 
 app.post('/categories', function(req, res) {
-	if(checkChildrenId(req)) {
-		client.query("insert into category(category_name) values('"+req.body.name+"')");
-		req.body.childrenIds.forEach(insertAssoc);
-		res.send({"ok":true});
-	} else {
-		res.send({"ok":true,"error":"invalid Children"});
-	}
-
+	checkChildrenId(req, function(result) {
+		if (result) {
+			client.query("insert into category(category_name) values('"+req.body.name+"')");
+			req.body.childrenIds.forEach(insertAssoc);
+			res.send({"ok":true});
+			//res.send(checkChildrenId(req));
+		} else {
+			res.send({"ok":false,"error":"invalid Children"});
+		}
+	})
 	//console.log("insert into category(category_name,father_id) values('"+req.body.name+"',"+idFather+")")
 	
 })
@@ -56,35 +56,39 @@ app.listen(3000, function () {
 });
 
 
-//fix this
-function getNextId() {
-	if (jsonFile.categories.length == 0) {
-		return 1;
-	} else {
-		var lastId = jsonFile.categories[jsonFile.categories.length-1].id;
-		return lastId + 1;
-	}
-	
-}
-
-function checkChildrenId(requisition){
-	var ids ="";
+function checkChildrenId(requisition, callback){
+	var ids =""
 	var arrayChildrenIds = requisition.body.childrenIds;
+	var a;
+	var b;
+	if (arrayChildrenIds.length == 0) {
+		return true;
+	}
 	for (var i = 0; i < arrayChildrenIds.length-1; i++) {
+		console.log("ids: " + ids);
 		ids = ids.concat(arrayChildrenIds[i]+",");
 	}
 	ids = ids.concat(arrayChildrenIds[arrayChildrenIds.length-1]);
-
-	var checkChildrenId = client.query("SELECT COUNT(*) FROM category WHERE id_category in ("+ids+")",function(err,result){
+	console.log("ids: " + ids);
+	
+	client.query("SELECT COUNT(*) FROM category WHERE id_category in ("+ids+")",function(err,result){
 		if (err) {
 			console.log(err.message);
-		} else {
-			var checkIdChildren = parseInt(result.rows[0].count);
-			console.log(arrayChildrenIds.length == checkIdChildren);
-			return (arrayChildrenIds.length == checkIdChildren);
-		}
+		} 
+			var checkChildrenId = parseInt(result.rows[0].count);
+			console.log("checkChildrenId: " + checkChildrenId);
+			console.log("arrayChildrenIds: " + arrayChildrenIds.length);
+			console.log(arrayChildrenIds.length == checkChildrenId);
+			a = arrayChildrenIds.length;
+			b = checkChildrenId;
+			callback(arrayChildrenIds.length == checkChildrenId);
+			
 	});
-}	
+}
+
+function getQueryResult(arrayChildrenIds) {
+
+}
 		
 function insertAssoc(item) {
 	
