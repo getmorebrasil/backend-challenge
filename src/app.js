9eaf9859ebf9850a999f977d03c62bc46d27d3bd
Gabriel Baldessar <gb.baldessar@gmail.com>
2018-04-15 22:03:56 -0300

    - Fix app.js dependence

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var jsonFile = require("../data/jsonFile.json"); 
 
 var app = express();
 app.use(bodyParser.json());
 
-//return a list with all categories(still need to work on the presentation)
 app.get('/categories', function (req, res) {
   var JsonRes = JSON.stringify(jsonFile, 1, 4);
   res.send(JsonRes);
 });
 
-//filter categories by Id
 app.get('/categories/:id',function(req,res,next) {
 	var id = req.params.id;
 	var wantedCategorie = jsonFile.categories.filter(function(category) {
@@ -21,16 +20,14 @@ app.get('/categories/:id',function(req,res,next) {
 	res.send(wantedCategorie);
 })
 
-//register the new category in the Json
 app.post('/categories', function(req, res) {
-	//jsonFile = JSON.stringify(jsonFile);
-	var addedCategory = {"id":getNextId(), name:(req.body.name) ,childrenIds: req.body.childrenIds };//Generate a new category
-	if (checkChildrenId(addedCategory)) {// check if there is at least one invalid children
-		jsonFile.categories.push(addedCategory);//if children ok: push the new category to Json
-		fs.writeFile("../data/jsonFile.json",JSON.stringify(jsonFile));//saves the file
-		res.send({"ok":true});//return ok
+	if (checkChildrenId(req.body.childrenIds)) {
+		var addedCategory = {"id":getNextId(), name:(req.body.name) ,childrenIds: req.body.childrenIds };//Generate a new category
+		jsonFile.categories.push(addedCategory);
+		fs.writeFile("../data/jsonFile.json",JSON.stringify(jsonFile));
+		res.send({"ok":true});
 	} else {
-		res.send({"ok":false,"error":"invalid Children"});//id children not ok: return the error
+		res.send({"ok":false,"error":"invalid Children"});
 	}
 	
 	
@@ -53,22 +50,17 @@ function getNextId() {
 }
 
 function checkChildrenId(addedCategory){
-	var childrenArray = addedCategory.childrenIds;
-	if (childrenArray.length == 0) { // leaf category can be added
+	//var childrenArray = addedCategory.childrenIds;
+	if (addedCategory.length == 0) {
 		return true;
 	} else {
 		var childrenExists = true;
 		var wantedChildren;
-		//do the search for each element in childrenArray
-		for (var i = 0; i < childrenArray.length; i++) {
-			//console.log("childrenArray: ",childrenArray[i]);
+		for (var i = 0; i < addedCategory.length; i++) {
 			wantedChildren = jsonFile.categories.filter(function(category) {
-				return category.id == childrenArray[i];//compare the children Id with the categories Id in the Json
+				return category.id == addedCategory[i];
 			});
-			//console.log("tamanho: ",wantedChildren.length);
-			//console.log(wantedChildren);
-			if (wantedChildren.length == 0) { //if one of the children have no match in Json file it can't be added
-				//console.log('retornou false');
+			if (wantedChildren.length == 0) {
 				return false;
 			}
 		}
