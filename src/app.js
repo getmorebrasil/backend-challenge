17ae74d238751b8a5ffeb8bb2801cc7692191209
Gabriel Baldessar <gb.baldessar@gmail.com>
2018-04-17 02:34:06 -0300

    - FIX post and get route

--- src/app.js
+++ src/app.js
@@ -12,9 +12,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.get('/categories', function (req, res) {
-  //var JsonRes = (jsonFile);
- 
-  client.query("select (array_to_json(array_agg(row_to_json(arrayField)))) as categories from (select id_category, category_name from category) arrayField",function(err,result){
+  client.query("SELECT (array_to_json(array_agg(row_to_json(arrayField)))) AS categories FROM (SELECT name, ARRAY(SELECT id_child FROM father_children_assoc WHERE id_father = category.id ) as \"childrenIds\" from category) arrayField",function(err,result){
   	if (err) {
   		res.send(err.message);
   	}  
@@ -26,7 +24,7 @@ app.get('/categories', function (req, res) {
 
 app.get('/categories/:id',function(req,res,next) {
 	var id = req.params.id;
-	client.query("select (array_to_json(array_agg(row_to_json(arrayField)))) as categories from (select id_category, category_name from category) arrayField WHERE id_category = "+id+"",function(err,result){
+	client.query("select (array_to_json(array_agg(row_to_json(arrayField)))) as categories from (select id, name from category) arrayField WHERE id = "+id+"",function(err,result){
   	if (err) {
   		res.send(err.message);
   	}  
@@ -38,18 +36,18 @@ app.get('/categories/:id',function(req,res,next) {
 
 app.post('/categories', function(req, res) {
 	checkChildrenId(req, function(result) {
+		console.log(result);
 		if (result) {
-			client.query("insert into category(category_name) values('"+req.body.name+"')");
+			client.query("insert into category(name) values('"+req.body.name+"')");
 			req.body.childrenIds.forEach(insertAssoc);
+			console.log("Adicionou: " + req.body.name);
 			res.send({"ok":true});
 			//res.send(checkChildrenId(req));
 		} else {
 			res.send({"ok":false,"error":"invalid Children"});
 		}
-	})
-	//console.log("insert into category(category_name,father_id) values('"+req.body.name+"',"+idFather+")")
-	
-})
+	});
+});
 
 app.listen(3000, function () {
   console.log('listening on port 3000!');
@@ -57,43 +55,28 @@ app.listen(3000, function () {
 
 
 function checkChildrenId(requisition, callback){
-	var ids =""
+	var ids = 0;
 	var arrayChildrenIds = requisition.body.childrenIds;
-	var a;
-	var b;
-	if (arrayChildrenIds.length == 0) {
-		return true;
-	}
-	for (var i = 0; i < arrayChildrenIds.length-1; i++) {
-		console.log("ids: " + ids);
-		ids = ids.concat(arrayChildrenIds[i]+",");
-	}
-	ids = ids.concat(arrayChildrenIds[arrayChildrenIds.length-1]);
-	console.log("ids: " + ids);
-	
-	client.query("SELECT COUNT(*) FROM category WHERE id_category in ("+ids+")",function(err,result){
+	var queryString = "SELECT COUNT(*) FROM category WHERE id = ANY('{"+arrayChildrenIds+"}')";
+
+	client.query(queryString,function(err,result){
 		if (err) {
-			console.log(err.message);
-		} 
-			var checkChildrenId = parseInt(result.rows[0].count);
-			console.log("checkChildrenId: " + checkChildrenId);
-			console.log("arrayChildrenIds: " + arrayChildrenIds.length);
-			console.log(arrayChildrenIds.length == checkChildrenId);
-			a = arrayChildrenIds.length;
-			b = checkChildrenId;
-			callback(arrayChildrenIds.length == checkChildrenId);
+			console.log("Error: " + err.message);
+		} else if(arrayChildrenIds.length == 0) {
+			console.log("else if: " + arrayChildrenIds.length+" / " + result.rows[0].count);
+			return callback(true);
+		} else {
+			console.log("else : " + arrayChildrenIds.length+" / " + result.rows[0].count);
+			return callback(arrayChildrenIds.length == result.rows[0].count);
+		}
 			
 	});
-}
-
-function getQueryResult(arrayChildrenIds) {
-
 }
 		
 function insertAssoc(item) {
 	
 	try {
-		client.query("insert into father_children_assoc(id_father,id_child) values((select max(id_category) from category),"+item+")");
+		client.query("insert into father_children_assoc(id_father,id_child) values((select max(id) from category),"+item+")");
 	}
 	catch(err) {
 		console.log('err.message')
