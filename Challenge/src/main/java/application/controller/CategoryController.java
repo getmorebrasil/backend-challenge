567fe396e00ceb06e0bdbcee83d0e3042717f4c0
chelo84 <marcelo_lacroix@hotmail.com>
2018-05-17 13:27:00 -0300

    Changed path for POST/GET requests
    Changed the URL path for the POST and GET requests from ''http://localhost:{port}/'' on both to:
    
    ''http://localhost:{port}/categories/'' for POST;
    ''http://localhost:{port}/categories/'' for GET without parameters and;
    ''http://localhost:{port}/categories/{id}'' for GET with id as parameter.
    
    Also changed GET method from only one to two methods, one without parameters and another with id parameters in the URL and added prettify to JSON response.

--- Challenge/src/main/java/application/controller/CategoryController.java
+++ Challenge/src/main/java/application/controller/CategoryController.java
@@ -7,10 +7,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.category.Category;
@@ -21,34 +21,30 @@ public class CategoryController {
 	@Autowired
     private CategoryRepository repository;
     
-    @RequestMapping("/")
-    public ResponseEntity<ArrayList<Category>> getCategory(@RequestParam(value = "id", required=false) Long id) {
+	@RequestMapping(value="/categories/{id}", method=RequestMethod.GET)
+    public ResponseEntity<ArrayList<Category>> getCategory(@PathVariable(value = "id", required=false) Long id) {
     	final ArrayList<Category> categories = new ArrayList<>();
 
-    	if(id != null) {
-	    	repository.findById(id)
-	    					.ifPresent(category -> {
-	    						categories.add(category);
-	    					});
-	    	
-    	} else {
-    		categories.addAll(getAllCategories());
-    	}
+    	repository.findById(id)
+    		.ifPresent(category -> {
+    			categories.add(category);
+    		});
     	
     	return new ResponseEntity<ArrayList<Category>>(categories, HttpStatus.OK);
     }
     
-    public ArrayList<Category> getAllCategories() {
+	@RequestMapping(value="/categories", method=RequestMethod.GET)
+    public ResponseEntity<ArrayList<Category>> getAllCategories() {
     	ArrayList<Category> categories = new ArrayList<>();
     	
 		for(Category category : repository.findAll()) {
 			categories.add(category);
 		}
 		
-		return categories;
+    	return new ResponseEntity<ArrayList<Category>>(categories, HttpStatus.OK);
     }
     
-    @RequestMapping(value="/", method=RequestMethod.POST)
+    @RequestMapping(value="/categories", method=RequestMethod.POST)
     public ResponseEntity<Category> post(@Valid @RequestBody Category category) {
     	repository.save(category);
     	return new ResponseEntity<Category>(category, HttpStatus.OK);
