0577be0fb69a58572bd6d6181a86fd61e59d3b1f
chelo84 <marcelo_lacroix@hotmail.com>
2018-05-18 12:04:48 -0300

    Changed 'application.properties' file location
    Changed the file location because maven wasn't finding it when building the jar file

--- Challenge/src/main/java/application/Application.java
+++ Challenge/src/main/java/application/Application.java
@@ -2,6 +2,7 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class Application {
