2e526b7c85b395748a984707cdfe557f8de1ba62
Daniel Scariot <danielsguterres@gmail.com>
2019-01-09 19:47:46 -0200

    API Consegue salvar no banco de dados
    Necessita arrumar o atributo childrensIds, pois nada impede de colocar um id que não exista.
    Falta fazer o get pelo ID da categoria

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const Client = require('pg').Client;
 require('dotenv').config();
-
-
 
 const app = express();
 const mustache = mustacheExpress();
@@ -12,7 +12,7 @@ mustache.cache = null;
 app.engine('mustache', mustache);
 app.set('view engine', 'mustache');
 
-
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static('public'));
 
 app.get('/list', (req,res) => {
@@ -21,6 +21,31 @@ app.get('/list', (req,res) => {
 
 app.get('/categories', (req,res) => {
 	res.render('categories');
+});
+
+app.post('/categories', (req,res) => {
+	console.log('post body', req.body);
+	const client = new Client();
+	client.connect()
+		.then(() => {
+			console.log('connection complete');
+			if (req.body.childrenlds) {
+				const sql = 'INSERT INTO categorias (id, name, childrenlds) VALUES ($1, $2, $3)'
+				var params = [req.body.id, req.body.name, req.body.childrenlds];
+				return client.query(sql, params);
+			}else
+				const sql = 'INSERT INTO categorias (id, name, childrenlds) VALUES ($1, $2, $3)'
+				var params = [req.body.id, req.body.name, {}];
+				return client.query(sql, params);
+		})
+		.then((result) => {
+			console.log('result?', result);
+			res.redirect('/list');
+		})
+		.catch((err) => {
+			console.log('err', err)
+			res.redirect('/list');
+		});
 });
 
 app.listen(process.env.PORT, function() {
