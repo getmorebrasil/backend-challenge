b20899adbbf0db33dbd3b74aee974a916cba4082
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2019-01-19 17:43:18 -0200

    index de rests implementado

---
+++ rest/index.ts
@@ -0,0 +1,33 @@
+import {Source} from "../events/Source";
+import {MainRest} from "./MainRest";
+
+const RestsObject = {
+    main: MainRest
+};
+
+export class InitRest extends Source {
+    private _rests: object;
+
+    constructor(router) {
+        super();
+        this.rests = RestsObject;
+
+        for (let rest in this.rests) {
+            if (this.rests.hasOwnProperty(rest))
+                new this.rests[rest](router);
+        }
+
+        process.nextTick(() => {
+            this.hub.send(this, `rest.ready`, {succes: null, error: null});
+        });
+    }
+
+    set rests (value: object) {
+        this._rests = value;
+    }
+
+
+    get rests(): object {
+        return this._rests;
+    }
+}
