afb58413c22d4fece6f17f9dc3b4963d604deea3
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2019-01-19 19:13:16 -0200

    Main Handler criado e funcoes utilizadas no rest criadas, ainda necessarios implementacao;

---
+++ handlers/MainHandler.ts
@@ -0,0 +1,16 @@
+import {BasicHandler} from "./util/BasicHandler";
+
+class MainHandler extends BasicHandler{
+
+    async getCategories () {
+        //todo implementar funcao de ler todas as categorias do banco
+    }
+
+    async getCategoriesById() {
+        //todo implementar funcao de ler categorias pelo id
+    }
+
+    async createCategory() {
+        //todo implementar funcao de criar categoria
+    }
+}
