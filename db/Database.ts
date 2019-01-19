f423d9d831d379c587b023f1f39aa46f472cdd61
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2019-01-19 12:51:29 -0200

    Classe Database implementada, esta é a classe responsavel por iniciar o Banco

--- db/Database.ts
+++ db/Database.ts
@@ -1,7 +1,11 @@
+import * as fs from "fs";
+import * as path from 'path';
+import {managers} from "./index";
 import {Mongoose} from "mongoose";
+import * as mongoose from "mongoose";
 import {Source} from "../events/Source";
 import {ManagerMap} from "../interfaces/ManagerMap";
-import * as mongoose from "mongoose";
+
 
 export class Database extends Source {
 
@@ -16,11 +20,60 @@ export class Database extends Source {
 
         this.mongoose.connection.on('error', (err, val) => console.log('error', err, val));
 
-        this.hub.on('db.getManager', this.getManager.bind(this))ç
+        this.hub.on('db.getManager', this.getManager.bind(this));
         this.init(config.mongodb);
     }
 
     private async init(config) {
-
+        try {
+            await this.mongoose.connect(`mongodb://${config.host}/${config.name}`);
+            if (config.eraseDb)
+                await this.databasePopulate();
+            this.hub.send(this, 'banco.ready', {success: true, error: false});
+        } catch (err) {
+            console.error(err);
+            process.exit(1)
+        }
     }
+
+    async databasePopulate() {
+        await this.mongoose.connection.db.dropDatabase();
+        let dirPath = path.resolve('test/fixtures');
+        let files = fs.readdirSync(dirPath);
+        let promises = [];
+        while(files.length > 0) {
+            let file = require(path.join(dirPath,files.shift()));
+            promises.push(this.hub.send(this, `db.${file.model}.create`, {
+                succes: file.data,
+                error: null
+            }).promise);
+        }
+
+        await Promise.all(promises);
+        promises = [];
+        for (let idx in this.managers) {
+            if (this.managers.hasOwnProperty(idx)) {
+                promises.push(this.managers[idx].model.ensureIndexes());
+            }
+        }
+
+        await Promise.all(promises);
+    }
+
+    async destroy() {
+        try {
+            await this.mongoose.connection.close();
+        } catch (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    }
+
+    getManager(msg) {
+        if (msg.source === this.id) return;
+
+        let manager = msg.data.success;
+        this.hub.send(this, 'db.getManagers', this.managers[manager], msg.id);
+    }
+
 }
