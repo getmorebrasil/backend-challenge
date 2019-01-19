ceb1ad85d1e2ac20fda934344411cac5fb953f4d
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2019-01-19 19:18:06 -0200

    rotas criadas e funcoes para ligar Rest ao handler tambem Porem nao implementadas

--- rest/MainRest.ts
+++ rest/MainRest.ts
@@ -8,13 +8,26 @@ export class MainRest extends BasicRest{
 
         this.routes = {
             get: {
-                "/rota": this.function.bind(this),
+                "/categories": this.getCategories.bind(this),
+                "/categories/:id": this.getCategoriesById.bind(this),
             },
             post: {
-                "/rota": this.function.bind(this),
+                "/categories": this.createCategory.bind(this),
             }
         };
         this.wiring();
+    }
+
+    async createCategory(req: Request, res: any) {
+        //todo implementar funcao de ligar Rest ao handler
+    }
+
+    async getCategories(req: Request, res: any) {
+        //todo implementar funcao de ligar Rest ao handler
+    }
+
+    async getCategoriesById(req: Request, res: any) {
+        //todo implementar funcao de ligar Rest ao handler
     }
 
 
