9409666d53f2066fddfd7084627ab5d4dac5cca6
manfio <manfio.leopoldo@gmail.com>
2019-01-21 21:45:43 -0200

    funções de ler categorias implementadas e começo de criar categorias implementadas

--- handlers/MainHandler.ts
+++ handlers/MainHandler.ts
@@ -1,16 +1,43 @@
 import {BasicHandler} from "./util/BasicHandler";
+import {QueryObject} from "./util/QueryObject";
 
-class MainHandler extends BasicHandler{
+export class MainHandler extends BasicHandler{
 
-    async getCategories () {
-        //todo implementar funcao de ler todas as categorias do banco
+    async getCategoriesByCode (req) {
+        let queryObj = new QueryObject(req.params, 'treeHeight');
+        let treeHeight = await this.emitToServer('db.category.read', queryObj);
+        if (treeHeight.data.success.length < 1)
+            return this.handlReturn('category', 'nonexistent', MainHandler.ERROR(treeHeight.data));
+        treeHeight = treeHeight.data.success[0].treeHeight;
+
+        let populate = this.setNestedPopulate(treeHeight);
+        queryObj = new QueryObject(req.params, 'code name childrendIds childrenCodes', populate);
+        let ret = await this.emitToServer('db.category.read', queryObj);
+        return this.handlReturn('category', 'errorReturn', ret.data);
     }
 
-    async getCategoriesById() {
-        //todo implementar funcao de ler categorias pelo id
+    async getCategories() {
+        let queryObj = new QueryObject({root: true}, 'childrenCodes childrenIds code name treeHeight');
+        queryObj.populate = this.setNestedPopulate(3);
+        let ret = await this.emitToServer('db.category.read', queryObj);
+
+        return this.handlReturn('category', 'errorReturn', ret.data);
     }
 
-    async createCategory() {
+    private setNestedPopulate(treeHeight: number) {
+        let populate:{path: string, select: string, populate: any} =
+            {path: 'childrenIds', select: 'childrenCodes code childrenIds name', populate: ''};
+        for(let i = 0; i < treeHeight; i++) {
+            let pop = {path: 'childrenIds', select: 'childrenCodes code childrenIds name',
+                populate: {path: 'childrenIds', select: 'childrenCodes code childrenIds name', populate: ''}};
+            populate['populate'] = pop;
+        }
+        return populate;
+    }
+
+    async createCategory(req) {
         //todo implementar funcao de criar categoria
+        let ret = await this.emitToServer('db.category.create', req.body);
+        return this.handlReturn('category', '', ret.data);
     }
 }
