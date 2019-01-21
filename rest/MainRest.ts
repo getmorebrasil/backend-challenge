b8a01da26db221a8895201e528cb25f3b54856ee
manfio <manfio.leopoldo@gmail.com>
2019-01-21 21:45:51 -0200

    funções de ler categorias implementadas e começo de criar categorias implementadas

--- rest/MainRest.ts
+++ rest/MainRest.ts
@@ -1,15 +1,21 @@
 import {BasicRest} from "./BasicRest";
+import {MainHandler} from "../handlers/MainHandler";
+
+const HTTPStatus = require('http-status-codes');
 
 
 export class MainRest extends BasicRest{
 
+    private _handler: MainHandler;
+
     constructor(router) {
         super(router);
+        this.handler = new MainHandler();
 
         this.routes = {
             get: {
                 "/categories": this.getCategories.bind(this),
-                "/categories/:id": this.getCategoriesById.bind(this),
+                "/categories/:code": this.getCategoriesByCode.bind(this),
             },
             post: {
                 "/categories": this.createCategory.bind(this),
@@ -18,16 +24,36 @@ export class MainRest extends BasicRest{
         this.wiring();
     }
 
+    set handler(value: MainHandler) {
+        this._handler = value;
+    }
+
+    get handler() :MainHandler {
+        return this._handler;
+    }
+
     async createCategory(req: Request, res: any) {
         //todo implementar funcao de ligar Rest ao handler
+        let ret = await this.handler.createCategory(req);
+
+        if(ret.error)
+            return res.status(HTTPStatus.BAD_REQUEST).send(ret)
+        return;
     }
 
     async getCategories(req: Request, res: any) {
-        //todo implementar funcao de ligar Rest ao handler
+        let ret = await this.handler.getCategories();
+        if (ret.error)
+            return res.status(HTTPStatus.BAD_REQUEST).send(ret);
+        return res.status(HTTPStatus.OK).send(ret.data);
     }
 
-    async getCategoriesById(req: Request, res: any) {
-        //todo implementar funcao de ligar Rest ao handler
+    async getCategoriesByCode(req: Request, res: any) {
+        let ret = await this.handler.getCategoriesByCode(req);
+        if(ret.error)
+            return res.status(HTTPStatus.BAD_REQUEST).send(ret);
+        else
+            return res.status(HTTPStatus.OK).send(ret.data);
     }
 
 
