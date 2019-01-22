e8eacd4c8687eddfddf37b4fb4657445f7f2d7c9
manfio <manfio.leopoldo@gmail.com>
2019-01-22 16:45:23 -0200

    Testes de Criar categorias finalizados

--- test/rest/MainRestTest.ts
+++ test/rest/MainRestTest.ts
@@ -93,9 +93,8 @@ describe('MainRest Test', () => {
                    .send(category)
                    .end((err, res) => {
                        expect(res.status).to.be.eqls(400);
-                       expect(res.body.error).to.include.keys('code');
-                       expect(res.body.error.code).to.include.all.keys('buttons', 'description', 'title', 'type');
-                       expect(res.body.error.code.type).to.be.eqls('mongoCode11000');
+                       expect(res.body.error).to.include.all.keys('buttons', 'description', 'title', 'type');
+                       expect(res.body.error.type).to.be.eqls('mongoCode11000');
 
                        done();
                    });
@@ -111,21 +110,134 @@ describe('MainRest Test', () => {
                    .post('/api/categories')
                    .send(category)
                    .end((err, res) => {
-                       //todo expects
                        expect(res.status).to.be.eqls(400);
-                       expect(res.body.error).to.include.keys('code');
-                       expect(res.body.error.code).to.include.all.keys('buttons', 'description', 'title', 'type');
-                       expect(res.body.error.code.type).to.be.eqls('mongoCode11000');
+                       expect(res.body.error).to.include.all.keys('buttons', 'description', 'title', 'type');
+                       expect(res.body.error.type).to.be.eqls('nonexistentChildren');
 
                        done();
                    });
            });
 
-           it('Com Parâmetros corretos', () => {
-                //todo
-           });
+           describe('Esportes - parametros corretos', () => {
 
-       })
+               it('Bolas', () => {
+                   const category = {
+                       name: 'Bolas',
+                       code: 311,
+                   };
+                   chai.request('http://localhost:2051')
+                       .post('/api/categories')
+                       .send(category)
+                       .end((err, res) => {
+                           expect(res.status).to.be.eqls(201);
+                           expect(res.body).to.be.instanceOf(Array);
+                           expect(res.body[0]).to.include.keys('id', 'name', 'code', 'childrenIds', 'childrenCodes');
+                           expect(res.body[0].code).to.be.eqls(311);
+                       });
+               });
+
+               it('Chuteiras', () => {
+                   const category = {
+                       name: 'Chuteiras',
+                       code: 312,
+                   };
+                   chai.request('http://localhost:2051')
+                       .post('/api/categories')
+                       .send(category)
+                       .end((res, err) => {
+                           expect(res.status).to.be.eqls(201);
+                           expect(res.body).to.be.instanceOf(Array);
+                           expect(res.body[0]).to.include.keys('id', 'name', 'code', 'childrenIds', 'childrenCodes');
+                           expect(res.body[0].code).to.be.eqls(312);
+                       });
+               });
+
+               it('Acessórios', () => {
+                   const category = {
+                       name: 'Acessórios',
+                       code: 313,
+                   };
+                   chai.request('http://localhost:2051')
+                       .post('/api/categories')
+                       .send(category)
+                       .end((res, err) => {
+                           expect(res.status).to.be.eqls(201);
+                           expect(res.body).to.be.instanceOf(Array);
+                           expect(res.body[0]).to.include.keys('id', 'name', 'code', 'childrenIds', 'childrenCodes');
+                           expect(res.body[0].code).to.be.eqls(313);
+                       });
+               });
+
+               it('Cola', () => {
+                   const category = {
+                       name: 'Cola',
+                       code: 321,
+                   };
+                   chai.request('http://localhost:2051')
+                       .post('/api/categories')
+                       .send(category)
+                       .end((res, err) => {
+                           expect(res.status).to.be.eqls(201);
+                           expect(res.body).to.be.instanceOf(Array);
+                           expect(res.body[0]).to.include.keys('id', 'name', 'code', 'childrenIds', 'childrenCodes');
+                           expect(res.body[0].code).to.be.eqls(321);
+                       });
+               });
+
+               it('Futebol', () => {
+                   const category = {
+                       name: 'Futebol',
+                       code: 31,
+                       childrenCodes: [311, 312, 313]
+                   };
+                   chai.request('http://localhost:2051')
+                       .post('/api/categories')
+                       .send(category)
+                       .end((res, err) => {
+                           expect(res.status).to.be.eqls(201);
+                           expect(res.body).to.be.instanceOf(Array);
+                           expect(res.body[0]).to.include.keys('id', 'name', 'code', 'childrenIds', 'childrenCodes');
+                           expect(res.body[0].code).to.be.eqls(31);
+                       });
+               });
+
+               it('Handebol', () => {
+                   const category = {
+                       name: 'Handebol',
+                       code: 32,
+                       childrenCodes: [321]
+                   };
+                   chai.request('http://localhost:2051')
+                       .post('/api/categories')
+                       .send(category)
+                       .end((res, err) => {
+                           expect(res.status).to.be.eqls(201);
+                           expect(res.body).to.be.instanceOf(Array);
+                           expect(res.body[0]).to.include.keys('id', 'name', 'code', 'childrenIds', 'childrenCodes');
+                           expect(res.body[0].code).to.be.eqls(32);
+                       });
+               });
+
+               it('Esportes', () => {
+                   const category = {
+                       name: 'Esportes',
+                       code: 3,
+                       childrenCodes: [31, 32],
+                       root: true
+                   };
+                   chai.request('http://localhost:2051')
+                       .post('/api/categories')
+                       .send(category)
+                       .end((res, err) => {
+                           expect(res.status).to.be.eqls(201);
+                           expect(res.body).to.be.instanceOf(Array);
+                           expect(res.body[0]).to.include.keys('id', 'name', 'code', 'childrenIds', 'childrenCodes');
+                           expect(res.body[0].code).to.be.eqls(3);
+                       });
+               });
+
+           });
+       });
 
    });
 
