09ada04214ca38ec01f4f9ec3c2524661f9c4343
m1k4 <camposmrc01@gmail.com>
2019-11-16 16:13:32 -0300

    Modularize error handling
    Moved handling functions for another file in module controller

---
+++ src/controller/controller.js
@@ -0,0 +1,74 @@
+const Categorie = require('../model/Categories')
+
+async function verifyChildrenIds(childrenIds) {
+    console.log(childrenIds.length)
+    let allExists = true
+    for (let index = 0; index < childrenIds.length; index++) {
+        let id = childrenIds[index]
+        if (await Categorie.findOne({ id : id }) === null) {
+            console.log(`ERROR, The children with ${id} not exist`)
+            allExists = false
+        }
+    }
+    console.log(`OK, all children exists`)
+    return allExists
+}
+
+async function verifyId(id) {
+    if (await Categorie.findOne({ id }) === null) {
+        console.log(`OK, The category with ${id} not exist`)
+        return true
+    } else {
+        console.log(`ERROR, The category with ${id} exist`)
+        return false
+    }
+}
+
+async function insertCategorie(req, res) {
+    const id = req.body.id
+    const childrenIds = req.body.childrenIds
+
+    if (await verifyId(id) && await verifyChildrenIds(childrenIds)) {
+        try {
+            
+            const categorie = new Categorie({
+                id: req.body.id,
+                name: req.body.name,
+                childrenIds: req.body.childrenIds
+            })
+
+            await categorie.save()
+            res.status(201).json({ ok: true })
+        } catch (err) {
+            res.json({ message: err })
+        }
+    } else {
+        res.json({ ok : false, error : "InvalidCategories" })
+    }
+}
+
+const filterResult = '-_id -__v'
+
+async function getAllCagetories(req, res) {
+    try {
+        const allCategories = await Categorie.find({}).select(filterResult).sort({ 'id' : -1})
+        res.json(allCategories)
+    } catch (err) {
+        res.json({ message: err })
+    }
+}
+
+async function getCategorieById(req, res) {
+    try {
+        const OneCategorie = await Categorie.findOne({ id : req.params.id }).select(filterResult)
+        res.json(OneCategorie)
+    } catch (err) {
+        res.json({ message: err })
+    }
+}
+
+module.exports = {
+    insertCategorie,
+    getAllCagetories,
+    getCategorieById
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,75 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const Categorie = require('../model/Categories')
-
-async function verifyChildrenIds(childrenIds) {
-    console.log(childrenIds.length)
-    let allExists = true
-    for (let index = 0; index < childrenIds.length; index++) {
-        let id = childrenIds[index]
-        if (await Categorie.findOne({ id : id }) === null) {
-            console.log(`ERROR, The children with ${id} not exist`)
-            allExists = false
-        }
-    }
-    console.log(`OK, all children exists`)
-    return allExists
-}
-
-async function verifyId(id) {
-    if (await Categorie.findOne({ id }) === null) {
-        console.log(`OK, The category with ${id} not exist`)
-        return true
-    } else {
-        console.log(`ERROR, The category with ${id} exist`)
-        return false
-    }
-}
+const controller = require('../controller/controller')
 
 router.post('/', async (req, res) => {
-
-    const id = req.body.id
-    const childrenIds = req.body.childrenIds
-
-    if (await verifyId(id) && await verifyChildrenIds(childrenIds)) {
-        try {
-            
-            const categorie = new Categorie({
-                id: req.body.id,
-                name: req.body.name,
-                childrenIds: req.body.childrenIds
-            })
-
-            await categorie.save()
-            res.status(201).json({ ok: true })
-        } catch (err) {
-            res.json({ message: err })
-        }
-    } else {
-        res.json({ ok : false, error : "InvalidCategories" })
-    }
-    
+    await controller.insertCategorie(req, res)
 })
 
-const filterResult = '-_id -__v'
-
 router.get('/', async (req, res) => {
-    try {
-        const allCategories = await Categorie.find({}).select(filterResult).sort({ 'id' : -1})
-        res.json(allCategories)
-    } catch (err) {
-        res.json({ message: err })
-    }
+    await controller.getAllCagetories(req, res)
 })
 
 router.get('/:id', async (req, res) => {
-    try {
-        const OneCategorie = await Categorie.findOne({ id : req.params.id }).select(filterResult)
-        res.json(OneCategorie)
-    } catch (err) {
-        res.json({ message: err })
-    }
+    await controller.getCategorieById(req, res)
 })
-
 
 module.exports = app => app.use('/categories', router)
