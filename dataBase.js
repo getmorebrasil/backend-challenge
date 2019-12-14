bece79b16e65bd739a7387965ad578212297a324
milkfrog <contin.marcelo@gmail.com>
2019-12-14 14:41:52 -0200

    Add rules to saving products

--- dataBase.js
+++ dataBase.js
@@ -1,11 +1,28 @@
 const products = {}
 
 function saveProduct(product) {
-    if (!products[product.id]){
+    let arrayChildrens = JSON.parse(product.childrenIds)
+    let existChildren = (arrayChildrens.length > 0) ? true : false
+    let allProducts = getProducts()
+    let isValidChildrens = true
+    if (existChildren) {
+        let amountValidated = 0
+        for (let i = 0; i < allProducts.length; i++) {
+            if (arrayChildrens.includes(allProducts[i].id)) amountValidated += 1
+        }
+        if (amountValidated !== arrayChildrens.length) isValidChildrens = false
+    }
+    if (!products[product.id] && isInteger(product.id) && isValidChildrens){
+        product.id = parseInt(product.id)
+        product.childrenIds = arrayChildrens
         products[product.id] = product
-        return 1
+        return "ok"
+    } else if (!isInteger(product.id)) {
+        return "invalidId"
+    } else if (!isValidChildrens) {
+        return "InvalidCategories"
     } else {
-        return 2
+        return "idRepeated"
     }
 }
 
@@ -15,6 +32,10 @@ function getProduct(id) {
 
 function getProducts() {
     return Object.values(products)
+}
+
+function isInteger(value) {
+    return /^\d+$/.test(value);
 }
 
 module.exports = {saveProduct, getProduct, getProducts}
