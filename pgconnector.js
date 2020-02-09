8aa6cdf8893c92ec211f1cd970ac8ec4704782c1
MayconGesser <maycon.antonio.gesser@gmail.com>
2020-02-09 15:17:42 -0300

    Conserta função de insert

--- pgconnector.js
+++ pgconnector.js
@@ -1,13 +1,15 @@
 const {Pool, Client} = require('pg');
 
 const insert_statement = "INSERT INTO categories(name) VALUES ($1)";
+const update_parentage_statement = "UPDATE categories SET parent_id = ($1) WHERE id = ($2)";
 const select_statement = "SELECT * FROM categories";
 
 function get_connection(params = {}){
 
 	let client = new Client({
-		user: params.user || "postgresql",
+		user: params.user || "getmore",
 		host: params.host || "localhost",
+		password: 'nodejsbackend',
 		database: params.database || "GETMORE",
 		port: params.port || 5432
 	 });
@@ -15,15 +17,15 @@ function get_connection(params = {}){
 	return client;
 }
 
-function insert(name, childrenIds = []){
+function insert(name, childrenIds){
 
 	let client = get_connection();
-	client.query("BEGIN");
-	let res = client.query(insert_statement, [name]);
-	console.log(res);
-	client.query("COMMIT");
-	console.log("done");
-	client.end();
+	client.connect();
+	client.query(insert_statement, [name]).
+	then(result => {
+		client.end();
+	})
+	.catch(e => console.error(e.stack));
 }
 
 module.exports.insert = insert;
