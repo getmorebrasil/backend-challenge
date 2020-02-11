4d6074a94dc8f24f06a37e3e84a9c81c14a9992f
MayconGesser <maycon.antonio.gesser@gmail.com>
2020-02-11 19:33:41 -0300

    Commit para poder pegar código em outra máquina

--- pgconnector.js
+++ pgconnector.js
@@ -1,8 +1,8 @@
 const {Pool, Client} = require('pg');
 
-const insert_statement = "INSERT INTO categories(name) VALUES ($1)";
+const insert_statement = "INSERT INTO categories(name) VALUES ($1) RETURNING id";
 const update_parentage_statement = "UPDATE categories SET parent_id = ($1) WHERE id = ($2)";
-const select_statement = "SELECT * FROM categories";
+const select_statement = "SELECT * FROM categories WHERE id $1 OR name $2";
 
 function get_connection(params = {}){
 
@@ -20,17 +20,38 @@ function get_connection(params = {}){
 function insert(name, childrenIds, callback){
 
 	let client = get_connection();
-	let status = 0;
-	let message = "";
 	client.connect();
-	client.query(insert_statement, [name])
+	client.query(insert_statement, [name], (err, res) => {
+		if(childrenIds.length !== 0){
+			let parent_id = res.rows[0].id;
+			for(let i in childrenIds){
+				client.query(update_parentage_statement, [parent_id, childrenIds[i]]);
+			}			
+		}
+	})
 	.then(() => {
+
 		callback(null, 200, "Categoria " + name + " cadastrada com sucesso.");
 		client.end();
 	})
 	.catch(e => {
 		callback(e);
 		client.end();
+	})
+	
+	
+}
+
+function get(id, name = null){
+
+	let client = get_connection();
+	client.connect();
+	client.query(select_statement, [id, name], (err, res) => {
+		if(err){
+			throw err;
+		}
+		client.end();
+		return res.rows[0];
 	})
 }
 
